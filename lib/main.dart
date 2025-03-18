6af8b36fe5456aa4e5d6510f4a43e92d4bb476dd
ReindorfN <144750275+ReindorfN@users.noreply.github.com>
2025-03-18 11:51:42 +0000

    hmm

--- lib/main.dart
+++ lib/main.dart
@@ -7,110 +7,38 @@ import 'Screens/landing.dart';
 void main() {
   runApp(const MyApp());
 }
-import 'Screens/settings.dart';
-import 'screens/home_screen.dart';
-import 'Screens/profile.dart';
-import 'Screens/landing.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: "Page 1",
-        initialRoute:
-            '/', // This tells Flutter which route to show first when app starts
-        routes: {
-          '/': (context) =>
-              const LandingPage(), // Root route '/' maps to LandingPage
-          '/home': (context) =>
-              const HomeScreen(), // '/home' route maps to HomeScreen
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: OnboardingScreen(),
+    );
+  }
+}
+
+@override
+Widget build(BuildContext context) => MaterialApp(
+      title: "Page 1",
+      initialRoute: '/',
+      routes: {
+        '/': (context) {
+          Future.delayed(const Duration(seconds: 5), () {
+            Navigator.pushReplacementNamed(context, '/home');
+          });
+          return OnboardingScreen();
         },
-      );
-}
+        '/home': (context) => const HomeScreen(),
+      },
+    );
 
-class NavigationDrawer extends StatelessWidget {
-  const NavigationDrawer({super.key});
 class NavigationDrawer extends StatelessWidget {
   const NavigationDrawer({super.key});
 
   @override
-  Widget build(BuildContext context) => Drawer(
-        child: SingleChildScrollView(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            buildHeader(context),
-            buildMenuItems(context),
-          ],
-        )),
-      );
-
-  Widget buildHeader(BuildContext context) => Container(
-        padding: EdgeInsets.only(
-          top: MediaQuery.of(context).padding.top,
-        ),
-      );
-
-  Widget buildMenuItems(BuildContext context) => Container(
-        padding: const EdgeInsets.all(24),
-        child: Wrap(
-          runSpacing: 16,
-          children: [
-            ListTile(
-              leading: const Icon(Icons.home),
-              title: const Text("Home"),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HomeScreen()),
-                );
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.notifications),
-              title: const Text("Notifications"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: const Icon(Icons.favorite_outline),
-              title: const Text("Favourites"),
-              onTap: () {},
-            ),
-            const Divider(
-              color: Colors.lightBlue,
-            ),
-            ListTile(
-              leading: const Icon(Icons.person),
-              title: const Text("Profile"),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ProfileScreen()),
-                );
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.settings),
-              title: const Text("Settings"),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const SettingsScreen()),
-                );
-              },
-            )
-          ],
-        ),
-      );
   Widget build(BuildContext context) => Drawer(
         child: SingleChildScrollView(
             child: Column(
